bdc9377a9299bef7f727a3e3c42c44fae760c3d2
shanmaocc <47207787+shanmaocc@users.noreply.github.com>
2025-01-25 02:24:44 +0800

    commit

--- deploy.sh
+++ deploy.sh
@@ -23,14 +23,16 @@ cd docs/.vuepress/dist
 if [ -z "$GITHUB_TOKEN" ]; then
   # 手动部署
   msg='deploy'
-  githubUrl=git@github.com:oddfar/notes.git
+  githubUrl=git@github.com:shanmaocc/notes.git
 else
   # 自动部署
   msg='来自github actions的自动部署'
-  githubUrl=https://oddfar:${GITHUB_TOKEN}@github.com/oddfar/notes.git
-  git config --global user.name "oddfar"
-  git config --global user.email "oddfar@163.com"
+  githubUrl=https://shanmaocc:${GITHUB_TOKEN}@github.com/shanmaocc/notes.git
+  git config --global user.name "shanmaocc"
+  git config --global user.email "shanmaocc@gmail.com"
 fi
+
+# 在 dist 目录下初始化一个新的 git 仓库
 git init
 git add -A
 git commit -m "${msg}"
@@ -43,30 +45,30 @@ rm -rf docs/.vuepress/dist
 #------------------------------------------
 
 
-#打包代码同步到 gitee gh-pages分支
-if [ -z "$SSH_PRIVATE_KEY" ]; then
-  echo '如果是空字符串，则不部署到gitee'
-else
-  #url访问目录
-  initDist "module.exports = '/'"
-  # 生成静态文件
-  npm run build
-  # 进入生成的文件夹
-  cd docs/.vuepress/dist
+# #打包代码同步到 gitee gh-pa.ges分支
+# if [ -z "$SSH_PRIVATE_KEY" ]; then
+#   echo '如果是空字符串，则不部署到gitee'
+# else
+#   #url访问目录
+#   initDist "module.exports = '/'"
+#   # 生成静态文件
+#   npm run build
+#   # 进入生成的文件夹
+#   cd docs/.vuepress/dist
 
-  giteeUrl=git@gitee.com:oddfar/notes.git  #gitee 仓库ssh地址
+#   giteeUrl=git@gitee.com:oddfar/notes.git  #gitee 仓库ssh地址
   
-  git config --global user.name "oddfar"
-  git config --global user.email "oddfar@163.com"
-  git init
-  git add -A
-  git commit -m "来自github actions的自动部署"
-  git push -f $giteeUrl master:gh-pages
+#   git config --global user.name "oddfar"
+#   git config --global user.email "oddfar@163.com"
+#   git init
+#   git add -A
+#   git commit -m "来自github actions的自动部署"
+#   git push -f $giteeUrl master:gh-pages
   
-  cd - # 退回开始所在目录
-  rm -rf docs/.vuepress/dist
-  # 删除秘钥
-  rm -rf ~/.ssh
-fi
+#   cd - # 退回开始所在目录
+#   rm -rf docs/.vuepress/dist
+#   # 删除秘钥
+#   rm -rf ~/.ssh
+# fi
 
 
